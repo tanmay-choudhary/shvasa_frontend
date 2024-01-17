916c4d5714a78c75825f4915830141fe1deab293
Tanmay <tanmaychoudhary4102@gmail.com>
2024-01-17 15:38:58 +0530

    add assign ticket

--- src/components/cards/TicketCard.js
+++ src/components/cards/TicketCard.js
@@ -1,32 +1,36 @@
 import React from "react";
 
-const TicketCard = ({ ticket }) => {
+const TicketCard = ({ ticket, assignTicket }) => {
   const formatDate = (dateString) => {
     const options = { year: "numeric", month: "long", day: "numeric" };
     return new Date(dateString).toLocaleDateString(undefined, options);
   };
+
   return (
-    <div className="bg-white border border-blue-400 shadow-md p-4 m-4 rounded-lg space-y-4">
-      {/*  <h1 className="text-2xl font-bold ">
-        <span className="text-blue-500 bg-blue-100 rounded-full px-5 py-2">
-          {ticket?.id}
-        </span>
-  </h1> */}
+    <div className="bg-white border border-blue-400 shadow-md p-4 m-4 rounded-lg space-y-4 relative">
+      <button
+        className="absolute top-2 right-2 bg-blue-500 text-white px-3 py-1 rounded-full"
+        onClick={() => {
+          assignTicket(ticket._id);
+        }}
+      >
+        Action
+      </button>
 
       <h3 className="text-xl font-bold mb-2">{ticket.topic}</h3>
-      <p className=" font-medium">
+      <p className="font-medium">
         Description: <span className="font-normal">{ticket.description}</span>
       </p>
-      <p className=" font-medium">
+      <p className="font-medium">
         Severity: <span className="font-normal">{ticket.severity}</span>
       </p>
-      <p className=" font-medium">
+      <p className="font-medium">
         Type: <span className="font-normal">{ticket.type}</span>
       </p>
-      <p className=" font-medium">
+      <p className="font-medium">
         Status: <span className="font-normal">{ticket.status}</span>
       </p>
-      <p className=" font-medium">
+      <p className="font-medium">
         Assigned To: <span className="font-normal">{ticket.assignedTo}</span>
       </p>
       <p className="font-medium">
@@ -34,7 +38,7 @@ const TicketCard = ({ ticket }) => {
         <span className="font-normal">{formatDate(ticket.dateCreated)}</span>
       </p>
       {ticket.resolvedOn && (
-        <p className=" font-medium">
+        <p className="font-medium">
           Resolved On: <span className="font-normal">{ticket.resolvedOn}</span>
         </p>
       )}
--- src/components/pages/Tickets.js
+++ src/components/pages/Tickets.js
@@ -11,6 +11,8 @@ const Tickets = () => {
   const [isFilterModalOpen, setFilterModalOpen] = useState(false);
   const [tickets, setTickets] = useState([]);
   const [boolean, setBoolean] = useState(true);
+  const [isSuccessModalOpen, setSuccessModalOpen] = useState(false);
+  const [successModalData, setSuccessModalData] = useState({});
 
   const openTicketModal = () => setTicketModalOpen(true);
   const closeTicketModal = () => setTicketModalOpen(false);
@@ -34,11 +36,27 @@ const Tickets = () => {
     closeTicketModal();
   };
 
+  const assignTicket = async (id) => {
+    try {
+      const response = await MakeApiCall("POST", "/api/assign-tickets", {
+        ticketId: id,
+      });
+
+      //console.log("Received data:", response.data);
+      //setTickets(response.data);
+      setSuccessModalData(response.data);
+      setSuccessModalOpen(true);
+      setBoolean(!boolean);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   const addTickets = async (obj) => {
     try {
       const response = await MakeApiCall("POST", "/api/support-tickets", obj);
 
-      console.log("Received data:", response.data);
+      //console.log("Received data:", response.data);
       //setTickets(response.data);
       setBoolean(!boolean);
     } catch (error) {
@@ -115,6 +133,22 @@ const Tickets = () => {
         <TicketForm onSubmit={submitTicketForm} onClose={closeTicketModal} />
       </ModalWrapper>
 
+      <ModalWrapper
+        isOpen={isSuccessModalOpen}
+        onClose={() => setSuccessModalOpen(false)}
+        label="Success Modal"
+      >
+        <div className="p-4">
+          <h2 className="text-2xl font-bold mb-4">
+            Ticket Successfully assigned to {successModalData.name}
+          </h2>
+          <p>Email: {successModalData.email}</p>
+          <p>Phone: {successModalData.phone}</p>
+          <p>Description: {successModalData.description}</p>
+          {/* Add more details as needed */}
+        </div>
+      </ModalWrapper>
+
       <FilterModal
         isOpen={isFilterModalOpen}
         onClose={closeFilterModal}
@@ -135,7 +169,7 @@ const Tickets = () => {
 
       {tickets.length > 0 ? (
         tickets.map((ticket, index) => (
-          <TicketCard key={index} ticket={ticket} />
+          <TicketCard key={index} ticket={ticket} assignTicket={assignTicket} />
         ))
       ) : (
         <p>No tickets available.</p>
