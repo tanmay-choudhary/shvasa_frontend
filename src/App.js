1965b419756172b2870397d2a78a999d84bcc3a9
Tanmay <tanmaychoudhary4102@gmail.com>
2024-01-17 18:34:23 +0530

    add route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/tickets" element={<Tickets />} />
         <Route path="/agents" element={<Agents />} />
+        <Route path="/" element={<Agents />} />
       </Routes>
     </Router>
   );
